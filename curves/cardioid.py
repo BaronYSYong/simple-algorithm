f51fa614f877bbfb46ee42ba48fb214900d8fd08
baron-Endeavor <baronysyong@gmail.com>
2017-07-13 17:20:17 +0900

    Add folder curves

---
+++ curves/cardioid.py
@@ -0,0 +1,6 @@
+from curves import Curves
+import math
+
+curve = Curves()
+x,y = curve.Cardioid(a=-0.7)
+curve.Plot2DGraph(x,y,'a=-0.7')
---
+++ curves/curves.py
@@ -0,0 +1,44 @@
+"""
+Reference:
+    https://elepa.files.wordpress.com/2013/11/fifty-famous-curves.pdf
+"""
+
+import matplotlib.pyplot as plt
+import math
+
+class Curves(object):
+    def deg2rad(self, pos):
+        return pos*math.pi/180.0  
+    
+    def Plot2DGraph(self, x, y, title):
+        plt.plot(x,y,'g')
+        plt.grid()
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.title(title)
+        plt.show()          
+        
+    def Lissajous(self, a, b, c, n):
+        t = range(-180,180)
+        x = []
+        y = []
+        for i in t:
+            i = self.deg2rad(i)
+            x.append(a*math.sin(n*i+c))
+            y.append(b*math.sin(i))
+        return x, y
+           
+    def Cardioid(self, a):
+        t = range(-180,180)
+        x = []
+        y = []
+        for i in t:
+            i = self.deg2rad(i)
+            x.append(a*(2*math.cos(i) - math.cos(2*i)))
+            y.append(a*(2*math.sin(i) - math.sin(2*i)))
+        return x, y
+        
+                  
+
+    
+    
